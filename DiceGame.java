b00c5cf20b82ba7de9128f5c8eceffa21c0f4608
Yuma0819 <y.kaminaga140819@u-aizu.ac.jp>
2023-06-15 11:04:22 +0900

    Dice game: initial revision

---
+++ DiceGame.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+
+class DiceGame{
+    public static void main(String args[]){
+	Random rand = new Random();
+	int Die1 = rand.nextInt(6) + 1;
+	int Die2 = rand.nextInt(6) + 1;
+
+	System.out.println("Rolling dice...");
+	System.out.println("Die 1: " + Die1);
+	System.out.println("Die 2: " + Die2);
+	int sum = Die1 + Die2;
+	System.out.println("Total value: " + sum);
+    }
+}
